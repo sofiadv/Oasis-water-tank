34c5b213ac209b755841740bbd9a06f6ab193650
sofiadv <sofia.dv@gmail.com>
2024-01-05 10:38:16 -0300

    agregue boton de whatsapp

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,22 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+// Ruta para la página de inicio
 router.get('/', (req, res) => {
-  res.render('index');  // Renderiza la vista EJS 'index.ejs'
+  res.render('index'); // Asegúrate de que 'index' coincida con el nombre de tu vista EJS
+});
+
+// Rutas para otras páginas
+router.get('/mision-vision', (req, res) => {
+  res.render('mision-vision');
+});
+
+router.get('/productos', (req, res) => {
+  res.render('productos');
+});
+
+router.get('/mayoristas', (req, res) => {
+  res.render('mayoristas');
 });
 
 module.exports = router;
